2b8190ae45b35b97b3f632f7320afdda4f81f1e7
VladSteopoaie <vlad.steopoaie@gmail.com>
2025-01-28 19:02:31 +0300

    Minor changes

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -13,9 +13,10 @@ module.exports = {
     hardhat: {
       chainId: 1337
     },
-    // sepolia: {
-    //   url: process.env.SEPOLIA_RPC_URL,
-    //   accounts: [process.env.PRIVATE_KEY],
-    // },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      // address: 0xC99C12Ce634745510FE5be34504F7eDbbC244Ffc
+    },
   },
 };
